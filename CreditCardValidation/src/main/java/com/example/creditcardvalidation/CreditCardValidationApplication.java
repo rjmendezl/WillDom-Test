52abc4efe384f4e0396d062834e9b6f77bb5a558
rjmendezl <00103719@uca.edu.sv>
2023-05-16 23:12:20 -0600

    :memo: Add: Base Source Code has been added.

---
+++ CreditCardValidation/src/main/java/com/example/creditcardvalidation/CreditCardValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditcardvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCardValidationApplication.class, args);
+    }
+
+}
---
+++ CreditCardValidation/src/test/java/com/example/creditcardvalidation/CreditCardValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creditcardvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
